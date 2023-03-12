b275f4b30d57da5d4174dae1fb05c0d46a0a6998
SotaSuzuki <3.3sota.suzuki@gmail.com>
2023-03-12 20:18:49 +0900

    setup react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
 import { useState, useEffect } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import "./index.css";
+
+const pages = import.meta.glob<true, string, any>("./pages/*.tsx", {
+  eager: true,
+});
+
+const routes = Object.keys(pages).map((path) => {
+  const name = path.match(/\.\/pages\/(.*)\.tsx$/)?.[1];
+  return {
+    name,
+    path: name === "index" ? "/" : `/${name}`,
+    component: pages[path].default,
+  };
+});
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,6 +29,15 @@ function App() {
 
   return (
     <div className="app">
+      <nav>
+        <ul>
+          {routes.map((route) => (
+            <li key={route.path}>
+              <Link to={route.path}>{route.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
@@ -26,13 +51,12 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        {routes.map(({ path, component: RouteComponent }) => (
+          <Route key={path} path={path} element={<RouteComponent />}></Route>
+        ))}
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function IndexPage() {
+  return (
+    <div>
+      <h1>Hello, Pages</h1>
+    </div>
+  );
+}
