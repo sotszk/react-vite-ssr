926f6b1922d501fc2b0d976959c091c6fc1ae3b5
SotaSuzuki <3.3sota.suzuki@gmail.com>
2023-03-12 20:53:19 +0900

    feat: add build command

--- server.js
+++ server.js
@@ -9,34 +9,55 @@ const PORT = 5173;
 // [x] import.meta.url には何が入るか
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-async function createServer() {
+async function createServer(
+  root = process.cwd(),
+  isProd = process.env.NODE_ENV === "production",
+) {
   const app = express();
 
-  // Vite server を Middleware モードで起動する
-  // appType は 'custom' で
-  const vite = await createViteServer({
-    server: { middlewareMode: true },
-    appType: "custom",
-  });
+  let vite;
 
-  // Vite の connect instance を Middleware として使う
-  app.use(vite.middlewares);
+  if (!isProd) {
+    // Vite server を Middleware モードで起動する
+    // appType は 'custom' で
+    vite = await createViteServer({
+      root,
+      server: { middlewareMode: true },
+      appType: "custom",
+    });
+
+    // Vite の connect instance を Middleware として使う
+    app.use(vite.middlewares);
+  } else {
+    app.use((await import("compression")).default());
+    app.use(
+      (await import("serve-static")).default(
+        path.resolve(__dirname, "dist/client"),
+        {
+          index: false,
+        },
+      ),
+    );
+  }
 
   app.use("*", async (req, res, next) => {
     const url = req.originalUrl;
 
     try {
-      // 1. index.html を読み込む
-      let template = fs.readFileSync(
-        path.resolve(__dirname, "index.html"),
-        "utf-8",
-      );
+      const templatePath = isProd
+        ? path.resolve(__dirname, "dist", "client", "index.html")
+        : path.resolve(__dirname, "index.html");
 
-      // 2. Vite HTML Transform を適用する。
-      template = await vite.transformIndexHtml(url, template);
+      let template = fs.readFileSync(templatePath, "utf-8");
+      let render;
 
-      // 3. server entry ファイルを読み込む
-      const { render } = await vite.ssrLoadModule("/src/entry-server.tsx");
+      if (!isProd) {
+        template = await vite.transformIndexHtml(url, template);
+        const ssrModule = await vite.ssrLoadModule("/src/entry-server.tsx");
+        render = ssrModule.render;
+      } else {
+        render = (await import("./dist/server/entry-server.js")).render;
+      }
 
       // 4. app HTML をレンダリングする
       const appHtml = await render(url);
