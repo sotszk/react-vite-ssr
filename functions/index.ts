b9020e7bc8e300877ade3888d272f5514ca3f883
SotaSuzuki <3.3sota.suzuki@gmail.com>
2023-03-13 16:02:58 +0900

    add log in function

--- functions/index.ts
+++ functions/index.ts
@@ -1,6 +1,7 @@
 import type { Handler } from "@netlify/functions";
 
-export const handler: Handler = async () => {
+export const handler: Handler = async (event, _context) => {
+  console.log("url:", event.rawUrl);
   return {
     statusCode: 200,
     body: "Hello, world",
